d9be2c8f1c798f65ce72b3ca05bb056fdcbcae2a
Believeharsh <bontinue@gmail.com>
2023-12-06 21:25:23 +0530

    formated

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,6 +1,7 @@
 // import React from 'react'
 import { MdDarkMode } from "react-icons/md";
 import { IoMenu } from "react-icons/io5";
+import { DiReact } from "react-icons/di";
 
 function Navigation() {
 
@@ -9,13 +10,17 @@ function Navigation() {
     <>
       <header className="shadow sticky z-50 top-0 bg-slate-900 pt-[2.5px] pb-[10px]">
         <nav className="pr-[10px] pl-[10px]">
-          <div className="flex flex-warp justify-between items-center border-b-[1.7px] border-white ">
+          <div className="flex flex-warp justify-between items-center border-b-[1.7px] border-white w-[92vw] m-auto ">
             <div className="">
               <ul className="text-white text-lg font-serif flex justify-between items-center py-1 ">
                 <li className="pr-[15px]">
                 <IoMenu className=" text-3xl" />
                 </li>
-                <li className="mx-1">Developer</li>
+                <div className="flex justify-center items-center">
+                <span className="text-2xl "><DiReact/></span>
+                <li className="mx-1 text-2xl font- "> Developer</li>
+                </div>
+
                 
               </ul>
             </div>
