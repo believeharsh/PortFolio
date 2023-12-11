a99728635859e887997f9142cfb861f5fc479770
Believeharsh <bontinue@gmail.com>
2023-12-11 15:09:14 +0530

    implemented dark mode

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -2,22 +2,31 @@
 import { MdDarkMode } from "react-icons/md";
 import { IoMenu } from "react-icons/io5";
 import { DiReact } from "react-icons/di";
+import { CiLight } from "react-icons/ci";
+import { useEffect, useState } from "react";
 
 function Navigation() {
-  const DarkMode = () => {
+  const [Theme, setTheme] = useState('dark');
 
-      document.documentElement.classList.add('dark')
-    
+  
+useEffect(()=> {
+  if(Theme === 'dark'){
+    document.documentElement.classList.add('dark')
   }
+  else document.documentElement.classList.remove('dark')
+},[Theme])
 
+const ToggleTheme = () => {
+  setTheme(Theme === 'dark' ? 'light' : 'dark')
+}
 
   return (
     <>
-      <header className="shadow sticky z-50 top-0 bg-slate-900 pt-[2.5px] pb-[10px] dark:bg-white">
+      <header className="shadow sticky z-50 top-0 dark:bg-slate-900 pt-[2.5px] pb-[10px] bg-white">
         <nav className="pr-[10px] pl-[10px]">
-          <div className="flex flex-warp justify-between items-center border-b-[1.7px] border-white w-[92vw] m-auto ">
+          <div className="flex flex-warp justify-between items-center border-b-[1.7px] dark:border-white w-[92vw] m-auto ">
             <div className="">
-              <ul className="text-white text-lg font-serif flex justify-between items-center py-1 ">
+              <ul className=" text-black dark:text-white text-lg font-serif flex justify-between items-center py-1 ">
                 <li className="pr-[15px]">
                 <IoMenu className=" text-3xl" />
                 </li>
@@ -31,9 +40,17 @@ function Navigation() {
             </div>
 
             <div className="">
-              <ul className="text-white text-lg font-serif flex justify-between items-start " >
-                <li className="mx-1 pr-5 text-7xl">
-                  <MdDarkMode  onClick={DarkMode} className="text-lg text-white" />
+              <ul className=" text-black dark:text-white text-lg font-serif flex justify-between items-start " >
+                <li className="mx-1 pr-5 text-7xl" onClick={ToggleTheme}>
+                
+                {Theme === 'dark' ? 
+                <MdDarkMode className="text-lg  text-black dark:text-white" /> : 
+                <CiLight  className="text-lg  text-black dark:text-white"/>
+                }
+                   
+                  
+                
+          
                 </li>
                
               </ul>
