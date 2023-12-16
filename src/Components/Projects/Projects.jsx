c26bdfdcddf54e9392175f45110ee85b7bea484b
Believeharsh <bontinue@gmail.com>
2023-12-16 22:43:55 +0530

    deleted fotter component

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,17 +1,7 @@
 function Projects () {
     return(
         <>
-        <div className="w-full p-10 bg-white dark:bg-slate-900">
-            <div className="w-[90vw] m-auto">
-              <div className="flex flex-wrap">
-              <div className="">
-                
-              </div>
-
-              </div>
-
-            </div>
-        </div>
+       
 
         </>
     )
