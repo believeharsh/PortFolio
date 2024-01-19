779054834826bb397abb2bb00f22357a5a91518d
Believeharsh <bontinue@gmail.com>
2024-01-19 15:04:37 +0530

    Working on routing

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,31 +2,46 @@
 
 export default function About() {
     return (
-        <div className="py-16 bg-white">
-            <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-                <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-                    <div className="md:5/12 lg:w-5/12">
-                        <img
-                            src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
-                            alt="image"
-                        />
+        // <div className="py-16 bg-white m-7">
+        //     <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+        //         <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+        //             <div className="md:5/12 lg:w-5/12">
+        //                 <img
+        //                     src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+        //                     alt="image"
+        //                 />
+        //             </div>
+        //             <div className="md:7/12 lg:w-6/12">
+        //                 <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+        //                     React development is carried out by passionate developers
+        //                 </h2>
+        //                 <p className="mt-6 text-gray-600">
+        //                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
+        //                     accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
+        //                     aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
+        //                 </p>
+        //                 <p className="mt-4 text-gray-600">
+        //                     Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
+        //                     Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
+        //                 </p>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+        <>
+            <div className="pt-17 bg-white mt-7 mx-5 ">
+                <div className="flex justify-center items-center">
+                   <div className="">
+                    <img src="https://placeholder.com/100" alt="" />
+                    <div className="">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, ducimus. Sequi, laborum nihil quibusdam earum, quaerat et a cupiditate tempore hic illo veritatis nemo modi placeat saepe, eveniet incidunt? Illo magni, consequuntur harum, rem delectus doloribus ipsam aliquid at nostrum corporis quam mollitia tempora dignissimos aut aperiam est vel, earum in eveniet distinctio necessitatibus recusandae voluptatum id eaque. Suscipit culpa mollitia ex hic. Nobis et soluta quos saepe sapiente vero accusamus tempore, provident, ex necessitatibus error, temporibus praesentium esse culpa illo libero veniam laboriosam vel magni facilis omnis quisquam officiis a. Dolorum harum voluptatibus officiis! Rerum quaerat repellat quod quis.</p>
                     </div>
-                    <div className="md:7/12 lg:w-6/12">
-                        <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
-                            React development is carried out by passionate developers
-                        </h2>
-                        <p className="mt-6 text-gray-600">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
-                            accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
-                            aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
-                        </p>
-                        <p className="mt-4 text-gray-600">
-                            Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
-                            Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
-                        </p>
-                    </div>
+                   </div>
+                   <div className=""></div>
+                   <div className=""></div>
+                   <div className=""></div>
                 </div>
             </div>
-        </div>
+        </>
     );
   }
