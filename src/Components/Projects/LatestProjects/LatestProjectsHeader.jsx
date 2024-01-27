7d90d67a812ca5aebd269c806eeffdf12fcdee6e
Believeharsh <bontinue@gmail.com>
2024-01-27 13:51:26 +0530

    Worked on Projects Headers

--- src/Components/Projects/LatestProjects/LatestProjectsHeader.jsx
+++ src/Components/Projects/LatestProjects/LatestProjectsHeader.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function LatestProjectsHeader() {
   return (
-  <>
-    <div className="mb-2">
-              <p className=" text-black dark:text-white  text-4xl px-1 py-1">Latest Projects
-                <span className="border-b-[1px] border-black "></span>
-              </p>
+    <>
+      <div className="mb-3 mx-3 dark:bg-slate-800 shadow-sm dark:shadow-white shadow-black rounded-xl pl-5">
+        <p className=" text-black dark:text-white  text-2xl px-1 py-1">Latest Projects
+          <span className="border-b-[1px] border-black "></span>
+        </p>
 
-    </div>
-  </>
+      </div>
+    </>
   )
 }
 
