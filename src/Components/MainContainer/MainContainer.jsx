7cf499b9aa9eb7523f1177c17bec8cb52e559a91
Believeharsh <bontinue@gmail.com>
2024-01-31 22:22:02 +0530

    formated

--- src/Components/MainContainer/MainContainer.jsx
+++ src/Components/MainContainer/MainContainer.jsx
@@ -6,55 +6,43 @@ import { useState, useEffect, useRef } from "react";
 import Navigation from "../Navigation/Navigation";
 import Connect from "../Connect/Connect";
 
-
 export default function MainContainer() {
   const [sidebar, setSidebar] = useState(false);
-  const [CenterBlur, setCenterBlur] = useState(false)
-  const [ConnectMenu, setConnectMenu] = useState(false)
+  const [CenterBlur, setCenterBlur] = useState(false);
+  const [ConnectMenu, setConnectMenu] = useState(false);
 
-  
- const RefSideMenu = useRef()
+  const RefSideMenu = useRef();
 
-
-  const ToggleSideBar =  (event) => {
+  const ToggleSideBar = (event) => {
     event.preventDefault();
-    setSidebar(!sidebar)
-    setCenterBlur(!CenterBlur)
-  }
+    setSidebar(!sidebar);
+    setCenterBlur(!CenterBlur);
+  };
 
   const ToggleConnect = (event) => {
     event.preventDefault();
-    setConnectMenu(!ConnectMenu)
-  }
+    setConnectMenu(!ConnectMenu);
+  };
 
-
-  
-useEffect(() => {
-  const CloseSideBar = (e) => {
-    if(!RefSideMenu.current.contains(e.target)){
-      setSidebar(false)
-      console.log("hello")
-    }
-  
-  }
-  document.addEventListener("click", CloseSideBar)
-  return(
-    document.removeEventListener("click", CloseSideBar)
-  )
-    
-
-})
+  useEffect(() => {
+    const CloseSideBar = (e) => {
+      if (!RefSideMenu.current.contains(e.target)) {
+        setSidebar(false);
+        console.log("hello");
+      }
+    };
+    document.addEventListener("click", CloseSideBar);
+    return document.removeEventListener("click", CloseSideBar);
+  });
 
   return (
     <>
-    <Navigation  click={ToggleSideBar}  />
+      <Navigation click={ToggleSideBar} />
 
-      <div className={` bg-white dark:bg-slate-900 h-full relative  `}   >
-
+      <div className={` bg-white dark:bg-slate-900 h-full relative  `}>
         <div className="flex ">
-        
           <div className="">
-            <SideMenu sidebar={sidebar}  RefSideMenu={RefSideMenu}  />
+            <SideMenu sidebar={sidebar} RefSideMenu={RefSideMenu} />
           </div>
           <div className="">
             <Connect Connectmenu={ConnectMenu} />
@@ -63,11 +51,13 @@ useEffect(() => {
           <div className=" bg-white  dark:bg-slate-900 left-0 h-[1750px]  w-[44px] fixed z-[11] dark:shadow-sm dark:shadow-slate-100 ">
             <Mainleft ToggleConnect={ToggleConnect} />
           </div>
-          <div className={`  bg-white  dark:bg-gradient-to-r from-gray-800 via-slate-900 to-gray-800 rounded-xl w-[calc(100%-49px-49px)]  mx-auto mt-[63px] px-2 py-2 mb-1 border-[1.5px] dark:border-white  ${CenterBlur  ? 'blur-sm z-[10] overflow-hidden' : ''  }`}>
+          <div
+            className={`  bg-white  dark:bg-gradient-to-r from-gray-800 via-slate-900 to-gray-800 rounded-xl w-[calc(100%-49px-49px)]  mx-auto mt-[63px] px-2 py-2 mb-1 border-[1.5px] dark:border-white  ${
+              CenterBlur ? "blur-sm z-[10] overflow-hidden" : ""
+            }`}
+          >
             <div className="">
-
-              <Outlet  />
-
+              <Outlet />
             </div>
           </div>
 
@@ -75,7 +65,6 @@ useEffect(() => {
             <MainRight />
           </div>
         </div>
-
       </div>
     </>
   );
